adbc446b00b299bffc17421b9823d03fa7ae1d99
Vinay Yadav <ydavvinay@gmail.com>
2026-01-05 21:43:17 +0530

    initial clean commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Components/App"; // Components folder me App.js
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+// .header {
+//     display: flex;
+//     justify-content: space-between;
+//     border: 1px solid black;
+
+// }
+
+// .logo {
+//     width: 150px;
+// }
+
+// .nav-items >ul{
+//     display: flex;
+//     list-style-type: none;
+//     font-size: 24px;
+//     padding: 2px 20px;
+
+// }
+// .nav-items >ul > li{
+//     padding: 10px;
+//     margin: 10px;
+// }
